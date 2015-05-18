f4994d92757a1e6d9271cf572529b67a54e88499
Matthias <me@sigpwr.de>
2015-05-18 11:05:08 +0200

    Added TextView and Getter for device screen size

--- app/src/main/java/de/fhdo/fragmentation_demo/MainActivity.java
+++ app/src/main/java/de/fhdo/fragmentation_demo/MainActivity.java
@@ -1,8 +1,10 @@
 package de.fhdo.fragmentation_demo;
 
+import android.graphics.Point;
 import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -18,12 +20,18 @@ public class MainActivity extends ActionBarActivity {
         TextView t1 = (TextView)findViewById(R.id.tv01);
         TextView t2 = (TextView)findViewById(R.id.tv02);
         TextView t3 = (TextView)findViewById(R.id.tv03);
+        TextView t4 = (TextView)findViewById(R.id.tv04);
 
         t1.setText(Build.VERSION.RELEASE);
         t2.setText(Build.VERSION.CODENAME);
 
         Integer sdk = Build.VERSION.SDK_INT;
         t3.setText(sdk.toString());
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        t4.setText("Screen size: " + size.x + "x" + size.y + " Pixels");
     }
 
     @Override
